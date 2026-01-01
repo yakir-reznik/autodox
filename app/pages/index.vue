11b04c04498375dc00516dd225b6748a18696e50
Yakir Reznik <yakir.reznik@gmail.com>
2026-01-01 08:01:12 +0200

    init

---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+	<div class="p-24">
+		<h1>This is my app</h1>
+		<p class="text-blue-500">This is a paragraph with red text.</p>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	compatibilityDate: "2025-07-15",
+	devtools: { enabled: true },
+	modules: ["@nuxt/fonts", "@nuxt/icon", "@nuxtjs/tailwindcss"],
+	app: {
+		head: {
+			title: "Autodox",
+			htmlAttrs: { lang: "he", dir: "rtl" },
+			meta: [{ name: "description", content: "Some sort of app description for SEO" }],
+		},
+	},
+});
