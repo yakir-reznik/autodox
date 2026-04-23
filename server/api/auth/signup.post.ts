58db026f41a24b730be54cf4e2a6df4acf4a0f49
Yakir Reznik <yakir.reznik@gmail.com>
2026-04-23 21:25:13 +0300

    temporarily disable signups until we figure out security

--- server/api/auth/signup.post.ts
+++ server/api/auth/signup.post.ts
@@ -4,6 +4,12 @@ import { db } from "~~/server/db";
 
 
 export default defineEventHandler(async (event) => {
+	// TODO: re-enable once security model is figured out
+	throw createError({
+		statusCode: 503,
+		statusMessage: "Signups temporarily disabled",
+	});
+
 	const body = await readBody(event);
 	const { name, email, password } = body;
 
